f9581f3de2527ea964558be26a41bfa7fbb52e81
geraldpasion <management@122.54.255.80>
2016-08-17 00:29:24 +0800

    test
    test

--- master_slot.php
+++ master_slot.php
@@ -1,19 +1,19 @@
 <?php
 include 'page_header.php';
-?>
 
-<?php
 echo "<form name=form1 id=form1 method=post>";
 echo "<input type='hidden' id='status' name='status' value=''>";
 if(!empty($_POST['status']))
 {
     $sms_slot=$_POST['sms_slot'];
     $phone_number=$_POST['phone_number'];
+    $sms_slot_name=$_POST['sms_slot_name'];
+    $sms_slot_order=$_POST['sms_slot_order'];
     $select="select slot_id from sms_slot_file order by slot_id desc limit 1";
      $result = $conn->query($select);
    	$row = $result->fetch_assoc();
    	$slot_id=$row['slot_id']+1;
-    $result=insertMaker('sms_slot_file',array('slot_id','sms_slot','phone_number'),array($slot_id,$sms_slot,$phone_number));
+    $result=insertMaker('sms_slot_file',array('slot_id','sms_slot','phone_number','sms_slot_order','sms_slot_name'),array($slot_id,$sms_slot,$phone_number,$sms_slot_order,$sms_slot_name));
        echo "<script>alert('Successfully Added New SMS Slot');
        
        document.getElementById('form1').action = 'sms_slot_list.php';
@@ -27,26 +27,48 @@ if(!empty($_POST['update_btn']))
     $sms_slot=$_POST['sms_slot'];
     $phone_number=$_POST['phone_number'];
     $slot_id=$_POST['slot_id'];
-    $result=updateMaker('sms_slot_file',array('sms_slot','phone_number'),array($sms_slot,$phone_number)," where slot_id='$slot_id'");
+    $sms_slot_name=$_POST['sms_slot_name'];
+    $sms_slot_order=$_POST['sms_slot_order'];
+    $result=updateMaker('sms_slot_file',array('sms_slot','phone_number','sms_slot_order','sms_slot_name'),array($sms_slot,$phone_number,$sms_slot_order,$sms_slot_name)," where slot_id='$slot_id'");
     //echo $result;
    echo "<script>alert('Successfully Updated New SMS Slot');
         document.getElementById('form1').action = 'sms_slot_list.php';
-         //   document.form1.submit();
+            document.form1.submit();
     </script>";
 }
 
 $trans_num="";
 $sms_slot="smsc";
 $phone_number="";
+$sms_slot_name="";
+$order="";
+$slot_id=0;
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 if(!empty($_REQUEST['trans_num']))
 {
     $slot_id=$_REQUEST['trans_num'];
-    $select="select sms_slot,phone_number from sms_slot_file where slot_id='$slot_id' limit 1";
-    $result = $conn->query($select);
-    $row = $result->fetch_assoc();
-    $sms_slot=$row['sms_slot'];
-    $phone_number=$row['phone_number'];
-    echo "<input type='hidden' id='slot_id' name='slot_id' value='$slot_id'>";
+    if(substr($slot_id,0,1)=="a")
+    {
+    	
+    	$order=str_replace("a","",$slot_id);
+    	$sms_slot="smsc".str_replace("a","",$slot_id);
+    	$slot_id=0;
+    	$sms_slot_name=$sms_slot;
+    }
+    else
+    {
+    
+    	$select="select sms_slot,phone_number,sms_slot_name,sms_slot_order from sms_slot_file where slot_id='$slot_id' limit 1";
+    	$result = $conn->query($select);
+    	$row = $result->fetch_assoc();
+    	$sms_slot=$row['sms_slot'];
+    	$phone_number=$row['phone_number'];
+    	$sms_slot_name=$row['sms_slot_name'];
+    	$order=$row['sms_slot_order'];
+    	echo "<input type='hidden' id='slot_id' name='slot_id' value='$slot_id'>";
+    }
+    
 }
 ?>
 
@@ -55,13 +77,16 @@ if(!empty($_REQUEST['trans_num']))
         <th style='text-align:left'><h2>SMS Slot</h2></th>
     </tr>
     <?php
-    echo textMaker('SMS Slot','sms_slot',$sms_slot);
+    echo "<tr><th style='text-align:left'>SMS Slot</th><td>".$sms_slot_name."</td></tr>";
+    echo "<input type='hidden' id='sms_slot_name' name='sms_slot_name' value='$sms_slot_name'>";
+    echo "<input type='hidden' id='sms_slot_order' name='sms_slot_order' value='$order'>";
+    echo textMaker('SMS Slot Name','sms_slot',$sms_slot);
     echo textMaker('Phone Number','phone_number',$phone_number);
     echo "<tr>";
         echo "<td colspan=2 style='text-align:center'>";
             
 
-		if(empty($_REQUEST['trans_num']))
+		if($slot_id==0)
             echo "<input type='button'  onclick='submit_btn_slot()'  name='submit_btn' value='Submit' style='margin:15px'>";
             else
             echo "<input type='submit' name='update_btn' value='Update' style='margin:15px'>";
--- sms_slot_list.php
+++ sms_slot_list.php
@@ -28,11 +28,27 @@ include 'page_header.php';
             document.form1.submit();
     	}
     }
+    function test_list(phone,sms)
+    {
+    
+    	document.getElementById('status').value="test"
+        document.getElementById('form1').action = 'sms_slot_list.php?phone='+phone+"&sms="+sms
+            document.form1.submit();
+    }
+    
 </script>
 <form id='form1' name='form1' method=post>
 <?php
 
-if(!empty($_REQUEST['status']) )
+
+
+if(!empty($_REQUEST['status']) &&$_REQUEST['status']=='test')
+{
+	$phone_number=$_REQUEST['phone'];
+	$smsc_id=$_REQUEST['sms'];
+	sendText("Test",$phone_number,$smsc_id,0);
+}
+else if(!empty($_REQUEST['status']) )
 {
     $status=1;
     if($_REQUEST['status']=="deactivate")
@@ -76,9 +92,8 @@ echo "<input type='hidden' name='status' id='status' >";
 echo "<input type='hidden' name='user_id' id='user_id' >";
 
 
-echo "<table>";
-echo "<table>";
-	echo "<tr>
+echo "<table class='table_data'>";
+	/*echo "<tr>
             	<td colspan=2>
                 	<ul class='nav nav-tabs'>
                     	<li $user_active role='presentation'><a href='".$page_name."?mas_status=1'>Active</a></li>
@@ -87,7 +102,7 @@ echo "<table>";
 		             </ul>
 		          </td>
 		    </tr>";
-
+*/
     echo "<tr>";
     $select="select count(id) as count from sms_slot_file where mas_status=1";
     $result = $conn->query($select);
@@ -106,7 +121,60 @@ echo "<table>";
         echo "</td>";
     echo "</tr>";
     echo "<tr>";
-        listMaker('sms_slot_file','sms_slot',array('sms_slot','phone_number','mas_status'),'SMS Slot List',$where);
+    
+    $select="SELECT * 
+	FROM information_schema.COLUMNS 
+	WHERE 
+    TABLE_SCHEMA = '$dbname' 
+	AND TABLE_NAME = 'sms_slot_file' 
+	AND COLUMN_NAME = 'sms_slot_order'";
+	$result = $conn->query($select);
+	$num_rows = mysql_affected_rows();
+	if($num_rows<=0)
+	{
+		$select="ALTER TABLE `sms_slot_file` ADD `sms_slot_order` INT NOT NULL ;";
+		$conn->query($select);
+		$select="ALTER TABLE `sms_slot_file` ADD `sms_slot_name` VARCHAR(10) NOT NULL ;";
+		$conn->query($select);
+		for($a=0;$a<$max_sms_slot;$a++)
+    	{
+			$update="update sms_slot_file set  sms_slot_order='$a',sms_slot_name='smsc$a' where mas_status=1 and sms_slot_name='' limit 1";
+			$conn->query($update);
+		}
+		
+	}
+	
+    $data=array();
+    $select="select * from sms_slot_file where mas_status=1 order by sms_slot_order";
+    $result = $conn->query($select);
+    while($row=$result->fetch_assoc())
+	{
+		$data[$row['sms_slot_order']]=array($row['sms_slot'],$row['phone_number'],$row['sms_slot_name'],$row['slot_id']);
+    }
+    $gsm_array=array('smsc0','smsc1','smsc2','smsc3');
+    echo "<tr>";
+    	echo "<th>Slot</th>";
+    	echo "<th>Slot Name</th>";
+    	echo "<th>Phone Number</th>";
+    
+    	echo "<th>Action</th>";
+    echo "</tr>";
+    for($a=0;$a<$max_sms_slot;$a++)
+    {
+    	$data2=array('','','',"a".$a);
+    	if(!empty($data[$a]))
+    	$data2=$data[$a];
+    	
+    		echo "<tr>";
+    			echo "<td>".$gsm_array[$a]."</td>";
+    			echo "<td>".$data2[0]."</td>";
+    			echo "<td>".$data2[1]."</td>";
+    			echo "<td><input type='button' value='EDIT' onclick='edit_list(\"".$data2[3]."\")'></TD>";
+    			echo "<td><input type='button' value='Test' onclick='test_list(\"".$data2[1]."\",\"".$gsm_array[$a]."\")'></TD>";
+    		echo"</tr>";
+    }
+    
+    //    listMaker('sms_slot_file','sms_slot',array('sms_slot','phone_number','mas_status'),'SMS Slot List',$where);
     
     echo "</tr>";
 echo "</table>";
